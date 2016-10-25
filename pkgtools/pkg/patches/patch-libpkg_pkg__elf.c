e0cc7ff29795daed55d664c43845fa9272d2c39f
John Marino (DF) <dragonflybsd@marino.st>
2016-10-24 20:45:02 -0700

    Sync: Mon Oct 24 20:44:13 PDT 2016

---
+++ pkgtools/pkg/patches/patch-libpkg_pkg__elf.c
@@ -0,0 +1,13 @@
+$NetBSD$
+
+--- libpkg/pkg_elf.c.orig	2016-07-05 15:06:13.000000000 +0000
++++ libpkg/pkg_elf.c
+@@ -787,6 +787,8 @@ pkg_get_myarch_elfparse(char *dest, size
+ #if defined(__DragonFly__)
+ 	snprintf(dest, sz, "%s:%d.%d",
+ 	    osname, version / 100000, (((version / 100 % 1000)+1)/2)*2);
++#elif defined(__NetBSD__)
++	snprintf(dest, sz, "%s:%d", osname, (version + 1000000) / 100000000);
+ #else
+ 	snprintf(dest, sz, "%s:%d", osname, version / 100000);
+ #endif
