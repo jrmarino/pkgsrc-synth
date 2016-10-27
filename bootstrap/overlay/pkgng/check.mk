9cc9db07a0ac1740720e9588f940b52e6ab4016c
John Marino (DF) <dragonflybsd@marino.st>
2016-10-27 13:53:39 -0700

    Sync: Thu Oct 27 13:52:47 PDT 2016

--- bootstrap/overlay/pkgng/check.mk
+++ bootstrap/overlay/pkgng/check.mk
@@ -1,25 +1,31 @@
 # $NetBSD$
 #
-
 # _pkgformat-check-vulnerable:
 #	Checks for known vulnerabilities in the package if a vulnerability
 #	file exists.
 #
+# TODO: create service to convert pkg-vulnerabilities to VULNXML format
+#       and update pkg to point there by default
+#       Until then, pkg-audit is either going to malfunction or not work
+#       at all since it's using FreeBSD data
+
 .if defined(NO_PKGTOOLS_REQD_CHECK)
 _pkgformat-check-vulnerable: .PHONY
 	${RUN}${DO_NADA}
 .else
+
 _pkgformat-check-vulnerable: .PHONY
+.  if ! defined(ALLOW_VULNERABLE_PACKAGES)
 	${RUN}\
-	_PKGVULNDIR=`${_EXTRACT_PKGVULNDIR}`; \
-	vulnfile=$$_PKGVULNDIR/pkg-vulnerabilities;		\
-	if ${TEST} ! -f "$$vulnfile"; then 			\
-		${PHASE_MSG} "Skipping vulnerability checks.";	\
-		${WARNING_MSG} "No $$vulnfile file found.";	\
-		${WARNING_MSG} "To fix run: \`${DOWNLOAD_VULN_LIST}'."; \
-		exit 0;						\
-	fi;							\
+	vulnfile=${PKG_DBDIR}/vuln.xml; \
+	if ${TEST} ! -f "$$vulnfile"; then \
+		${PHASE_MSG} "Skipping vulnerability checks"; \
+		${WARNING_MSG} "No '$$vulnfile' file found."; \
+		${WARNING_MSG} "To fix, run: 'pkg audit --fetch'"; \
+		exit 0; \
+	fi; \
 	${PHASE_MSG} "Checking for vulnerabilities in ${PKGNAME}"; \
-	${AUDIT_PACKAGES} ${_AUDIT_PACKAGES_CMD} ${AUDIT_PACKAGES_FLAGS} ${PKGNAME} \
-	|| ${FAIL_MSG} "Define ALLOW_VULNERABLE_PACKAGES in mk.conf or ${_AUDIT_CONFIG_OPTION} in ${_AUDIT_CONFIG_FILE}(5) if this package is absolutely essential."
+	${PKG_AUDIT_CMD} ${_AUDIT_PACKAGES_CMD} --recursive ${PKGNAME} \
+	|| ${FAIL_MSG} "Define ALLOW_VULNERABLE_PACKAGES in mk.conf if this package is absolutely essential."
+.  endif
 .endif
--- mk/pkgformat/pkgng/pkgformat-vars.mk
+++ mk/pkgformat/pkgng/pkgformat-vars.mk
@@ -11,13 +11,6 @@ PKGSRC_MESSAGE_RECIPIENTS?=	# empty
 USE_TOOLS+=	mail
 .endif
 
-.if defined(PKG_PRESERVE)
-USE_TOOLS+=	date
-.endif
-
-# This is the package database directory for the default view.
-PKG_DBDIR?=		/var/db/pkgng
-
 # _PKG_DBDIR is the actual packages database directory where we register
 # packages.
 #
@@ -26,62 +19,31 @@ _HOST_PKG_DBDIR=	${HOST_PKG_DBDIR:U${PKG_DBDIR}}
 
 PKG_CMD?=		${PKG_TOOLS_BIN}/pkg
 PKG_ADD_CMD?=		${PKG_CMD} add
-PKG_ADMIN_CMD?=		false
 PKG_CREATE_CMD?=	${PKG_CMD} create
 PKG_DELETE_CMD?=	${PKG_CMD} delete
 PKG_INFO_CMD?=		${PKG_CMD} info
-LINKFARM_CMD?=		${PKG_TOOLS_BIN}/linkfarm
+PKG_AUDIT_CMD=		${PKG_CMD} audit
 
-# Latest versions of tools required for correct pkgsrc operation.
-.if make(replace) && ${_USE_DESTDIR} != "no"
-PKGTOOLS_REQD=		20100914
-.else
-PKGTOOLS_REQD=		20090528
-.endif
+# This is the package database directory that pkg(8) is configured to use
+PKG_DBDIR!=		${PKG_CMD} config PKG_DBDIR
 
-# Latest version of pkg_install required to extract packages
-PKGTOOLS_VERSION_REQD=	20091115
-
-.if !defined(PKGTOOLS_VERSION)
-PKGTOOLS_VERSION!=	${PKG_INFO_CMD} -V 2>/dev/null || echo 20010302
-MAKEFLAGS+=		PKGTOOLS_VERSION=${PKGTOOLS_VERSION}
-.endif
-
-# Check that we are using up-to-date pkg_* tools with this file.
-.if !defined(NO_PKGTOOLS_REQD_CHECK) && ${PKGTOOLS_VERSION} < ${PKGTOOLS_REQD}
-BOOTSTRAP_DEPENDS+=	pkg_install>=${PKGTOOLS_REQD}:../../pkgtools/pkg_install
-_PKG_INSTALL_DEPENDS=	yes
-.endif
-
-AUDIT_PACKAGES?=	${PKG_ADMIN}
-_AUDIT_PACKAGES_CMD?=	audit-pkg
-_EXTRACT_PKGVULNDIR=	${PKG_ADMIN} config-var PKGVULNDIR
-DOWNLOAD_VULN_LIST?=	${PKG_ADMIN} fetch-pkg-vulnerabilities
-_AUDIT_CONFIG_FILE=	pkg_install.conf
-_AUDIT_CONFIG_OPTION=	IGNORE_URL
+# Hardcode PKGTOOLS_VERSION to latest at time of writing
+PKGTOOLS_VERSION=	20160410
 
 # The binary pkg_install tools all need to consistently refer to the
 # correct package database directory.
 #
-PKGTOOLS_ARGS?=		#-K ${_PKG_DBDIR}
 SET_DBDIR?=		${PKGSRC_SETENV} PKG_DBDIR="${_PKG_DBDIR}" \
 			INSTALL_AS_USER=yes
 HOST_SET_DBDIR?=	${PKGSRC_SETENV} PKG_DBDIR="${_HOST_PKG_DBDIR}" \
 			INSTALL_AS_USER=yes
 
-PKG_ADD?=	${SET_DBDIR} ${PKG_ADD_CMD}
-PKG_ADMIN?=	${PKG_ADMIN_CMD} # unsupported
-PKG_CREATE?=	${SET_DBDIR} ${PKG_CREATE_CMD}
-PKG_DELETE?=	${SET_DBDIR} ${PKG_DELETE_CMD}
-PKG_INFO?=	${SET_DBDIR} ${PKG_INFO_CMD}
-LINKFARM?=	${LINKFARM_CMD}
+PKG_ADD?=		${SET_DBDIR} ${PKG_ADD_CMD}
+PKG_CREATE?=		${SET_DBDIR} ${PKG_CREATE_CMD}
+PKG_DELETE?=		${SET_DBDIR} ${PKG_DELETE_CMD}
+PKG_INFO?=		${SET_DBDIR} ${PKG_INFO_CMD}
 
-HOST_PKG_ADD?=		${HOST_SET_DBDIR} ${PKG_ADD_CMD}
-HOST_PKG_ADMIN?=	${PKG_ADMIN_CMD} # unsupported
-HOST_PKG_CREATE?=	${HOST_SET_DBDIR} ${PKG_CREATE_CMD}
-HOST_PKG_DELETE?=	${HOST_SET_DBDIR} ${PKG_DELETE_CMD}
 HOST_PKG_INFO?=		${HOST_SET_DBDIR} ${PKG_INFO_CMD}
-HOST_LINKFARM?=		${LINKFARM_CMD}
 
 # "${_PKG_BEST_EXISTS} pkgpattern" prints out the name of the installed
 # package that best matches pkgpattern.  Use this instead of
@@ -89,7 +51,6 @@ HOST_LINKFARM?=		${LINKFARM_CMD}
 # package name.
 #
 _PKG_BEST_EXISTS?=	${PKG_INFO_CMD} --show-name-only --glob
-_HOST_PKG_BEST_EXISTS?=	${HOST_PKG_INFO} -Eg
 
 # XXX Leave this here until all uses of this have been purged from the
 # XXX public parts of pkgsrc.
