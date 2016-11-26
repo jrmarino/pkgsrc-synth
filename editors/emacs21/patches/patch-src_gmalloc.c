ba7f693c0727573a620fdc2409a3cfa14d4a544c
John Marino (DF) <dragonflybsd@marino.st>
2016-11-26 00:05:39 -0800

    Sync: Sat Nov 26 00:00:00 PST 2016

--- editors/emacs21/patches/patch-src_gmalloc.c
+++ editors/emacs21/patches/patch-src_gmalloc.c
@@ -1,20 +1,11 @@
-$NetBSD: patch-bi,v 1.2 2016/11/26 03:50:16 dholland Exp $
+$NetBSD: patch-src_gmalloc.c,v 1.1 2016/11/26 04:22:40 dholland Exp $
 
 Work around bug in gcc 5.x that makes calloc into a call to itself,
 resulting in an infinite loop.
 
---- src/gmalloc.c.orig	2000-02-21 13:21:51.000000000 +0000
+--- src/gmalloc.c.orig	2001-11-04 17:35:43.000000000 +0000
 +++ src/gmalloc.c
-@@ -371,7 +371,7 @@ __malloc_size_t _bytes_free;
- /* Are you experienced?  */
- int __malloc_initialized;
- 
--__malloc_size_t __malloc_extra_blocks;
-+__malloc_size_t __malloc_extra_blocks = 0;
- 
- void (*__malloc_initialize_hook) PP ((void));
- void (*__after_morecore_hook) PP ((void));
-@@ -1420,7 +1420,13 @@ calloc (nmemb, size)
+@@ -1520,7 +1520,13 @@ calloc (nmemb, size)
       register __malloc_size_t nmemb;
       register __malloc_size_t size;
  {
