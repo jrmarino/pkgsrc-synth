be88e679a35cd9e6ba8bc83f391a21ec7e821690
John Marino (DF) <dragonflybsd@marino.st>
2016-12-12 13:37:47 -0800

    Sync: Mon Dec 12 13:37:16 PST 2016

--- mk/alternatives.mk
+++ mk/alternatives.mk
@@ -40,13 +40,17 @@ ALTERNATIVES_SRC?=	# none
 
 .if !empty(ALTERNATIVES_SRC)
 
+# pkgsrc-synth hack, this needs USE_TOOLS+=pkg_alternatives:run
+DEPENDS+=	pkg_alternatives-[0-9]*:../../pkgtools/pkg_alternatives
+
 ${WRKDIR}/.altinstall: ${ALTERNATIVES_SRC}
 	@{ ${ECHO} 'if ${TEST} $${STAGE} = "POST-INSTALL"; then'; \
-	${ECHO} '${CAT} >./+ALTERNATIVES <<EOF'; \
+	${ECHO} 'if ${TEST} -x ${PKG_ALTERNATIVES}; then'; \
+	${ECHO} '${CAT} >/tmp/${PKGBASE}_ALTERNATIVES <<EOF'; \
 	${SED} ${FILES_SUBST_SED} <${ALTERNATIVES_SRC}; \
 	${ECHO} 'EOF'; \
-	${ECHO} 'if ${TEST} -x ${PKG_ALTERNATIVES}; then'; \
-	${ECHO} '${PKG_ALTERNATIVES} -gs register ./+ALTERNATIVES'; \
+	${ECHO} '${PKG_ALTERNATIVES} -gs register /tmp/${PKGBASE}_ALTERNATIVES'; \
+	${ECHO} '${RM} -f /tmp/${PKGBASE}_ALTERNATIVES'; \
 	${ECHO} 'fi'; \
 	${ECHO} 'fi'; \
 	} >${WRKDIR}/.altinstall
@@ -54,9 +58,12 @@ ${WRKDIR}/.altinstall: ${ALTERNATIVES_SRC}
 ${WRKDIR}/.altdeinstall: ${ALTERNATIVES_SRC}
 	@{ ${ECHO} 'if ${TEST} $${STAGE} = "DEINSTALL"; then'; \
 	${ECHO} 'if ${TEST} -x ${PKG_ALTERNATIVES}; then'; \
-	${ECHO} '${PKG_ALTERNATIVES} -gs unregister ./+ALTERNATIVES'; \
+	${ECHO} '${CAT} >/tmp/${PKGBASE}_ALTERNATIVES <<EOF'; \
+	${SED} ${FILES_SUBST_SED} <${ALTERNATIVES_SRC}; \
+	${ECHO} 'EOF'; \
+	${ECHO} '${PKG_ALTERNATIVES} -gs unregister /tmp/${PKGBASE}_ALTERNATIVES'; \
+	${ECHO} '${RM} -f /tmp/${PKGBASE}_ALTERNATIVES'; \
 	${ECHO} 'fi'; \
-	${ECHO} '${RM} -f ./+ALTERNATIVES'; \
 	${ECHO} 'fi'; \
 	} >${WRKDIR}/.altdeinstall
 
