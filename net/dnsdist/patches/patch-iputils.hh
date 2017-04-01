dddc5bd7bd0e60c26dd512a53fbc3574b5d390ca
John Marino (DF) <dragonflybsd@marino.st>
2017-03-31 18:25:12 -0700

    Sync: Fri Mar 31 18:00:00 PDT 2017

--- net/dnsdist/patches/patch-iputils.hh
+++ net/dnsdist/patches/patch-iputils.hh
@@ -1,22 +1,22 @@
-$NetBSD: patch-iputils.hh,v 1.3 2017/03/31 19:41:13 fhajny Exp $
+$NetBSD: patch-iputils.hh,v 1.1 2017/03/31 20:49:51 fhajny Exp $
 
-- IP_PKTINFO structure different on NetBSD than expected.
+- Do not use IP_PKTINFO on NetBSD, the structure is not as expected.
 - Backport fix for SunOS segfault problem, see:
   https://github.com/PowerDNS/pdns/pull/4877
 
---- iputils.hh.orig	2017-01-13 08:55:55.000000000 +0000
+--- iputils.hh.orig	2017-01-17 08:43:49.000000000 +0000
 +++ iputils.hh
-@@ -38,6 +38,10 @@
- #include <boost/tuple/tuple.hpp>
- #include <boost/tuple/tuple_comparison.hpp>
+@@ -40,6 +40,10 @@
  
-+#if defined(IP_PKTINFO) && defined(__NetBSD__)
+ #include "namespaces.hh"
+ 
++#if defined(__NetBSD__) && defined(IP_PKTINFO)
 +#undef IP_PKTINFO
 +#endif
 +
- #include "namespaces.hh"
- 
  #ifdef __APPLE__
+ #include <libkern/OSByteOrder.h>
+ 
 @@ -59,23 +63,22 @@
  #define le64toh(x) OSSwapLittleToHostInt64(x)
  #endif
