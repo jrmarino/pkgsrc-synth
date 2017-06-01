8c66f818a5d7e76e24bafe65ee4151816d54b94c
John Marino (DF) <dragonflybsd@marino.st>
2017-05-31 18:09:26 -0700

    Sync: Wed May 31 18:00:00 PDT 2017

--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -1,4 +1,4 @@
-#	$NetBSD: bsd.pkg.mk,v 1.2022 2017/05/04 18:30:56 joerg Exp $
+#	$NetBSD: bsd.pkg.mk,v 1.2024 2017/05/31 22:56:41 jlam Exp $
 #
 # This file is in the public domain.
 #
@@ -371,11 +371,6 @@ USE_TOOLS+=								\
 # bsd.wrapper.mk
 USE_TOOLS+=	expr
 
-# scripts/shlib-type
-.if ${_OPSYS_SHLIB_TYPE} == "ELF/a.out"
-USE_TOOLS+=	file
-.endif
-
 # INSTALL/DEINSTALL script framework
 .if ${PKG_FORMAT:Mpkgng}
 .include "pkginstall/bsd.pkgng-install.mk"
@@ -624,6 +619,12 @@ ${.CURDIR}/${WRKDIR_BASENAME}:
 # MAKEFLAGS.su-${.TARGET}
 #	The additional flags that are passed to the make process.
 #
+# PRE_CMD.su-${.TARGET}
+#	Shell command executed before running the command that requires
+#	root privileges.  This may "exit 0" to short-circuit the command
+#	list and skip executing the command that requires the root
+#	privileges.
+#
 
 _ROOT_CMD=	cd ${.CURDIR} &&					\
 		${PKGSRC_SETENV} ${PKGSRC_MAKE_ENV}				\
@@ -635,11 +636,7 @@ _ROOT_CMD=	cd ${.CURDIR} &&					\
 
 .PHONY: su-target
 su-target: .USE
-	${RUN} \
-	case ${PRE_CMD.su-${.TARGET}:Q}"" in				\
-	"")	;;							\
-	*)	${PRE_CMD.su-${.TARGET}} ;;				\
-	esac;								\
+	${RUN}${PRE_CMD.su-${.TARGET}:U${TRUE}};			\
 	if ${_IS_ROOT_CMD}; then					\
 		${_ROOT_CMD};						\
 	else								\
