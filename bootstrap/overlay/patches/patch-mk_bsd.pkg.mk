6c3ba02382b3e64e0e3c3e4b0de83828b839a7ce
John Marino (DF) <dragonflybsd@marino.st>
2017-06-07 14:50:47 -0700

    Sync: Wed Jun  7 14:50:43 PDT 2017

--- bootstrap/overlay/patches/patch-mk_bsd.pkg.mk
+++ bootstrap/overlay/patches/patch-mk_bsd.pkg.mk
@@ -1,14 +1,14 @@
---- mk/bsd.pkg.mk.orig	2016-11-08 20:05:53 UTC
+--- mk/bsd.pkg.mk.orig	2017-06-07 21:47:51 UTC
 +++ mk/bsd.pkg.mk
-@@ -377,7 +377,11 @@ USE_TOOLS+=	file
+@@ -381,7 +381,11 @@ USE_TOOLS+=	expr
+ .include "pkgtasks/bsd.pkgtasks.mk"
+ .else
+ # INSTALL/DEINSTALL script framework
++. if ${PKG_FORMAT:Mpkgng}
++.include "pkginstall/bsd.pkgng-install.mk"
++. else
+ .include "pkginstall/bsd.pkginstall.mk"
++. endif
  .endif
  
- # INSTALL/DEINSTALL script framework
-+.if ${PKG_FORMAT:Mpkgng}
-+.include "pkginstall/bsd.pkgng-install.mk"
-+.else
- .include "pkginstall/bsd.pkginstall.mk"
-+.endif
- 
  # Locking
- .include "internal/locking.mk"
--- mk/bsd.pkg.mk
+++ mk/bsd.pkg.mk
@@ -381,7 +381,11 @@ USE_TOOLS+=	expr
 .include "pkgtasks/bsd.pkgtasks.mk"
 .else
 # INSTALL/DEINSTALL script framework
+. if ${PKG_FORMAT:Mpkgng}
+.include "pkginstall/bsd.pkgng-install.mk"
+. else
 .include "pkginstall/bsd.pkginstall.mk"
+. endif
 .endif
 
 # Locking
