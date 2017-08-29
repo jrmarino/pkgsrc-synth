0832f38d8871d83b9ae187a1d12b18a5c531f270
John Marino (DF) <dragonflybsd@marino.st>
2017-08-29 06:02:58 -0700

    Sync: Tue Aug 29 06:00:00 PDT 2017

--- security/gpgme/buildlink3.mk
+++ security/gpgme/buildlink3.mk
@@ -1,4 +1,4 @@
-# $NetBSD: buildlink3.mk,v 1.26 2017/08/01 11:48:20 ryoon Exp $
+# $NetBSD: buildlink3.mk,v 1.28 2017/08/29 12:11:35 wiz Exp $
 
 BUILDLINK_TREE+=	gpgme
 
@@ -9,18 +9,14 @@ BUILDLINK_API_DEPENDS.gpgme+=	gpgme>=0.9.0
 BUILDLINK_ABI_DEPENDS.gpgme+=	gpgme>=1.8.0nb1
 BUILDLINK_PKGSRCDIR.gpgme?=	../../security/gpgme
 
-.include "../../devel/librfuncs/buildlink3.mk"
 .include "../../security/libassuan2/buildlink3.mk"
 .include "../../security/libgpg-error/buildlink3.mk"
 
 pkgbase:=		gpgme
 .include "../../mk/pkg-build-options.mk"
 
-.if !empty(PKG_BUILD_OPTIONS.gpgme:Mgnupg21)
-DEPENDS+=		gnupg21>=2.1:../../security/gnupg21
-GPGME_GPG=		${PREFIX}/bin/gpg2
-.elif !empty(PKG_BUILD_OPTIONS.gpgme:Mgnupg2)
-DEPENDS+=		gnupg2>=2.0<2.1:../../security/gnupg2
+.if !empty(PKG_BUILD_OPTIONS.gpgme:Mgnupg2)
+DEPENDS+=		gnupg2-[0-9]*:../../security/gnupg2
 GPGME_GPG=		${PREFIX}/bin/gpg2
 .else
 DEPENDS+=		gnupg>=1.4.2:../../security/gnupg
